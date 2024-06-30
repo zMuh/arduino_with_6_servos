53378205469d3b90c0ebc89d622e624aafd9d57c
Muhannd <85467517+zMuh@users.noreply.github.com>
2024-07-01 00:45:45 +0300

    Update arduino_with_6_servos.ino

--- arduino_with_6_servos.ino
+++ arduino_with_6_servos.ino
@@ -1,15 +1,3 @@
-// C++ code
-//
-/*
-  Sweep
-
-  by BARRAGAN <http://barraganstudio.com>
-  This example code is in the public domain.
-
-  modified 8 Nov 2013  by Scott Fitzgerald
-  http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 
 int pos = 0;
